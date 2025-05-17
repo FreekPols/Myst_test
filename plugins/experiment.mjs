cba50839101667abd573ec18294cceba78abf160
Freek Pols <c.f.j.pols@tudelft.nl>
2025-05-17 09:15:43 +0200

    test experiment plugin

--- plugins/experiment.mjs
+++ plugins/experiment.mjs
@@ -1,9 +1,7 @@
-/**
- * A custom admonition that uses a specific color, and is always a dropdown.
- */
+
 const experiment = {
   name: "experiment",
-  doc: "A custom admonition that uses a specific color, and is always a dropdown.",
+  doc: "A custom admonition that uses a specific color.",
   arg: { type: String, doc: "The title of the admonition." },
   options: {
     collapsed: { type: Boolean, doc: "Whether to collapse the admonition." },
@@ -12,14 +10,23 @@ const experiment = {
   run(data, vfile, ctx) {
       const admonition = {
         "type": "admonition",
-        "kind": "tip",
+        "kind": "error",
+        "icon": false,
         "children": [
+          // { 
+          //   "type": "text",
+          //   "value": "🧪",    
+          // },
+
           {
             "type": "admonitionTitle",
+            
             // The first ["children"][0] removes the MyST "tree" top-level node.
             // The second ["children"] removes an unnecessary top-level paragraph node.
-            "children": ctx.parseMyst(data.arg.trim())["children"][0]["children"]
+            "children": ctx.parseMyst(`🧪 ${data.arg.trim()}`)["children"][0]["children"]
+            
           },
+          
           {
             "type": "paragraph",
             "children": ctx.parseMyst(data.body.trim())["children"][0]["children"]
@@ -29,9 +36,13 @@ const experiment = {
     return [admonition];
   }
 };
+
 const plugin = {
   name: "experiment",
   directives: [experiment],
 };
 
+
+
 export default plugin;
+
